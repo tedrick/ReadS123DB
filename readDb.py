365805330a0bf1efa6474906165a597b22fb02ae
jame6423 <jtedrick@esri.com>
2022-08-04 11:34:39 -0700

    Comment cleanup

--- readDb.py
+++ readDb.py
@@ -7,7 +7,7 @@ Built-in globals will set the parameters to:
 '''
 import csv, sqlite3, json, os, sys, uuid
 
-#Constants for easy running
+# Constants for easy running
 IN_DB = r'./a084e65cd9403d813e8ec667ca329a63.sqlite'
 OUT_DIR = r'./out'
 
@@ -16,7 +16,7 @@ def shape2WKT(in_shape_data):
     # print(in_shape_data)
     out_text = ""
     if "rings" in in_shape_data:
-        #Polygons are lists of a list of coordinates
+        # Polygons are lists of a list of coordinates
         ring_texts = []
         for ring in in_shape_data["rings"]:
             out_text = out_text + "("
@@ -26,13 +26,13 @@ def shape2WKT(in_shape_data):
         out_text = "POLYGON ({0})".format("".join(ring_texts))
         pass
     elif "paths" in in_shape_data:
-        #Lines are a list of coordinates; Survey123 stores as if multiple segments are supported but only uses 1
+        # Lines are a list of coordinates; Survey123 stores as if multiple segments are supported but only uses 1
         path0 = in_shape_data["paths"][0]
         coordinate_text = ",".join([" ".join([str(value) for value in coordinate]) for coordinate in path0])
         out_text = "LINESTRING ({0})".format(coordinate_text)
         pass
     else:
-        #Assume point
+        # Assume point
         out_text = "POINT ({0} {1}".format(str(in_shape_data["x"]), str(in_shape_data["y"]))
         if "z" in in_shape_data and in_shape_data["z"]:
             out_text = out_text + " " + str(in_shape_data["z"])
@@ -60,7 +60,7 @@ def read_data(indata, parentglobalid=""):
         if parentglobalid != "":
             outFeature["data"]["parentglobalid"] = parentglobalid
         for fieldName, fieldValue in parentValuesDict.items():
-            #Test the key to see if it's a normal attribute, select_multiple (list), geometry (dict), repeat (list), or metadata (dict)
+            # Test the key to see if it's a normal attribute, select_multiple (list), geometry (dict), repeat (list), or metadata (dict)
             if isinstance(fieldValue, dict):
                 # process geometry
                 if "spatialReference" in fieldValue.keys():
@@ -73,14 +73,14 @@ def read_data(indata, parentglobalid=""):
                         if "z" in fieldValue.keys():
                             outFeature["data"][u"z_geometry"] = fieldValue["z"]
             elif isinstance(fieldValue, list) and len(fieldValue) > 0:
-                #Repeat - iterate through the repeats to generate their own records
+                # Repeat - iterate through the repeats to generate their own records
                 if isinstance(fieldValue[0], dict):
                     for record in fieldValue:
                         repeat_record = read_data({"{0}_{1}".format(parentTable, fieldName): record}, identifier)
                         outData.extend(repeat_record)
                 else:
                     if fieldValue[0] is not None:
-                    #Select_multiple - other item types; change to comma seperated list
+                    # Select_multiple - other item types; change to comma seperated list
                         outFeature["data"][fieldName] = ",".join(fieldValue)
             else:
                 outFeature["data"][fieldName] = fieldValue
@@ -93,19 +93,19 @@ def readS123db(inDB=IN_DB):
     cur = conn.cursor()
     surveys = []
     outTables = {}
-    #conn.text_factory = lambda x: x.decode("utf-16")
-    #Status indicates which 'box' teh Survey is in
-    #0 - Drafts
-    #1 - Outbox
-    #2 - Sent
-    #3 - Submission Error
-    #4 - Inbox
+    # conn.text_factory = lambda x: x.decode("utf-16")
+    # Status indicates which 'box' the Survey is in
+    # 0 - Drafts
+    # 1 - Outbox
+    # 2 - Sent
+    # 3 - Submission Error
+    # 4 - Inbox
     for row in cur.execute('SELECT name, data, status from Surveys where status = 1 or status = 3'):
-        ##print(row)
+        # print(row)
         print ('-----------------')
         surveyName = row[0]
         jFeature = json.loads(row[1])
-        ##print jFeature
+        # print(jFeature)
         features = read_data(jFeature)
         surveys.extend(features)
     for feature in surveys:
@@ -125,8 +125,8 @@ def writeCSV(surveys, outDir = OUT_DIR):
         surveyUpdates = surveyTransactions["updates"]
         updatefile = os.path.join(outDir, 'survey_{0}_updates.csv'.format(surveyName.encode('utf-8').decode('utf-8')))
 
-#        outfile=os.path.join(outDir, 'survey_{0}.csv'.format(surveyName.encode('utf-8').decode('utf-8')))
-#        outfile = ''.join(outfile.split()).encode('utf-8')
+        # outfile=os.path.join(outDir, 'survey_{0}.csv'.format(surveyName.encode('utf-8').decode('utf-8')))
+        # outfile = ''.join(outfile.split()).encode('utf-8')
         with open(addfile, 'w') as outFile:
             #get all possible fieldnames
             fieldnames = []
@@ -135,23 +135,23 @@ def writeCSV(surveys, outDir = OUT_DIR):
             fieldnamesSet = set(fieldnames)
             fieldnames = list(fieldnamesSet)
             writer = csv.DictWriter(outFile, fieldnames=fieldnames)
-#            writer = DictUnicodeWriter(outFile, fieldnames=fieldnames)
+            # writer = DictUnicodeWriter(outFile, fieldnames=fieldnames)
             writer.writeheader()
             for row in surveyAdds:
-                #print(u'{0}\t{1}'.format(surveyName, row))
+                # print(u'{0}\t{1}'.format(surveyName, row))
                 writer.writerow(row)
         with open(updatefile, 'w') as outFile:
-            #get all possible fieldnames
+            # get all possible fieldnames
             fieldnames = []
             for row in surveyUpdates:
                 fieldnames.extend(row.keys())
             fieldnamesSet = set(fieldnames)
             fieldnames = list(fieldnamesSet)
             writer = csv.DictWriter(outFile, fieldnames=fieldnames)
-#            writer = DictUnicodeWriter(outFile, fieldnames=fieldnames)
+            # writer = DictUnicodeWriter(outFile, fieldnames=fieldnames)
             writer.writeheader()
             for row in surveyUpdates:
-                #print(u'{0}\t{1}'.format(surveyName, row))
+                # print(u'{0}\t{1}'.format(surveyName, row))
                 writer.writerow(row)
 
 if __name__ == '__main__':
